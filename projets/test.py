517474349d5783eda26b84d0fff7febad8dc8fae
TranquillouMAN <beney7189@gmail.com>
2022-11-07 15:32:40 +0100

    test python

---
+++ projets/test.py
@@ -0,0 +1,22 @@
+#ECRIRE "bonsoir paris"
+r=12000
+s=1250
+e=10
+rh=230
+# assertionUn = (((365*3)/(24-(16-8)))*(rh)>r)==(e*s<r) #false
+# assOne = (15740,625 > 12000) #true
+# asstwo = (12500<12000) #false
+# assertionUn = (15740,625 > 12000)==(12500<12000) #false    
+# #assertion2 = (((365*3)/(4-(12-8)))*(rh)>r)==(e*s<r) #true
+# assOne = (1095/0>12000) #False
+# asstwo = (12500<12000) #false                                       
+
+
+def retournerSixPlusTrois():
+    return 6+3
+def retournerSixPlusX(x):
+    return 6 + x
+#print("Qui vole un" + retournerSixPlusTrois() + "vole un boeuf")
+print(retournerSixPlusX(3))
+
+#FIN   
