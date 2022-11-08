300ec8a519c3ea48e0c57e698d159df805bacc59
TranquillouMAN <beney7189@gmail.com>
2022-11-08 15:24:45 +0100

    exo

--- projets/test.py
+++ projets/test.py
@@ -32,15 +32,73 @@
 # 19.58jour ouvrée par mois
 # 8h ouvrée par jour
 
-def calculeSalaireSeconde(c,j,h,s):
-    return c/(j*h*s)
+# def calculeSalaireSeconde(Salaire,j,h,s):
+#     return Salaire/(j*h*s)
 
-print(calculeSalaireSeconde(1200,19.58,8,3600))
+# print(calculeSalaireSeconde(1200,19.58,8,3600))
 
-def calculeSalaireHeure(c,j,h):
-    return c/(j*h)
+# def calculeSalaireHeure(Salaire,j,h):
+# #     return Salaire/(j*h) 
 
-print(calculeSalaireHeure(1200,19.58,8))
+# # print(calculeSalaireHeure(1200,19.58,8))
         
+# # def calculeSalaireBrutEnNet(salaireBrut, coeff):
+# #     # Calcul du coefficient pour 24% de taxe sur les salaires
+# #     coeff= 1-(24/100)
+# #     # Calcul du salaire net
+# #     return salaireBrut * coeff
+
+# def withdrawFees(total, percent):
+#     # Calcul du montant des taxes a retirer
+#     fees = total * (percent / 100)
+#     #retourner la valeur totale moins les taxes
+#     return total - fees
+
+# def calculeSalaireNet(salaireBrut, coeff):
+#     # Calculer et reourner le salaire Net a partir du salire brut
+#     return withdrawFees(salaireBrut, coeff)
+
+# def divide(x,y):
+#     # si Y est égal a 0 alors la division est impossible
+#     if y==0:
+#     # alors renvoyer un message d'erreur
+#         print("Bah alors ?")
+#         return None
+#     #Sinon
+#     else:
+#     #retourner le calcul x/y
+#         return x/y
+
+# def input():
+    # renvoie un carctère de type string au hasard
+     
+
+
+# def miniJeux(char):
+#     char = input()
+#     # récupérer une lettre aléatoire et l'affecter a une variable
+#     #tant que le charactère n'est pas 'a' 
+#     while char != "a":
+#         # regénère un charactère aléatoire
+#         char = input()
+#         # si le charactère regénéré n'est pas 'a' alors la boucle continue
+#     print("gg")
+#     return
+#     # Affiche gg
+
+# tableau = [1,5,9,5,7,32,89]
+
+# #pour récupéré 7 je prends dans le tableau l'index 4
+# print(tableau[4])
+
+# len(tableau)
+# print(len(tableau))
+
+# prénom = "Dydy"
+# nom = "ben"
+# id = nom + ' ' + prénom
+
+# integerValue = 10
+# stringIntegerValue = str(10)
 
 #FIN   
