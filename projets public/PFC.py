79341075154e95498a67974217e34e582c2927ec
TranquillouMAN <beney7189@gmail.com>
2022-11-09 20:09:08 +0100

    modeDeJeux

--- projets public/PFC.py
+++ projets public/PFC.py
@@ -3,6 +3,21 @@
 # on admet la fonction random qui génère un nombre entre 0 et 2 
 # on admet la fonction input qui récup les entrés du jouer 
 
+# definir une fonction modeDeJeux avec comme paramètre joueur et bot
+    # afficher via le retour de l'execution de la fonction print de paramètre "souhaitez vous jouer contre un joueur ou contre un bot"
+    # assigner a la variable joueurOuBot le retour de l'execution de la fonction input 
+    # si joueurOuBot est égal a joueur 
+         # alors éxecute la fonction joueurContreJoueur
+    # si joueurOuBot est égal a bot
+        # alors éxecute la fonction joueurContreBot
+    # sinon retourner la fonction modeDeJeux
 
+# éxécuter la fonction modeDeJeux
+
+# definir une fonction joueurContreJoueur de paramètre pierre, papier ou ciseau
+    
+
+# definir une fonction joueurContreBot de paramètre pierre, papier ou ciseau
+      
 
 #FIN
