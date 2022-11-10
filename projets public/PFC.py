2a49877d8669426c9bc989c743fdcd518c047401
TranquillouMAN <beney7189@gmail.com>
2022-11-10 14:36:47 +0100

    pfc

--- projets public/PFC.py
+++ projets public/PFC.py
@@ -3,6 +3,45 @@
 # on admet la fonction random qui génère un nombre entre 0 et 2 
 # on admet la fonction input qui récup les entrés du jouer 
 
+# definir une fonction modeDeJeux avec comme paramètre joueur et bot
+    # afficher via le retour de l'execution de la fonction print de paramètre "souhaitez vous jouer contre un joueur ou contre un bot"
+    # assigner a la variable joueurOuBot le retour de l'execution de la fonction input 
+    # si joueurOuBot est égal a joueur 
+         # alors éxecute la fonction joueurContreJoueur
+    # si joueurOuBot est égal a bot
+        # alors éxecute la fonction joueurContreBot
+    # sinon retourner la fonction modeDeJeux
 
+# éxécuter la fonction modeDeJeux
+
+# definir une fonction joueurContreJoueur de paramètre pierre, papier ou ciseau
+    #assigner a la variable player1 le retour de l'execution de la fonction input de paramètre "Pierre, papier ou ciseau: "
+    #assigner a la variable player2 le retour de l'execution de la fonction input de paramètre "Pierre, papier ou ciseau: "
+    #tant que pointPlayer1 
+        # si player1 est égal a pierre
+            # si player2 est égal a pierre
+            # alors afficher via le retour de l'execution de la fonction print de paramètre "égalité"
+            # si player2 est égal a papier
+            # alors afficher via le retour de l'execution de la fonction print de paramètre "Le joueur 2 a gagner ce round"
+            # si player2 est égal a ciseau
+            # alors afficher via le retour de l'execution de la fonction print de paramètre "Le joueur 1 a gagner ce round"
+        # si player1 est égal a papier
+            # si player2 est égal a pierre
+            # alors afficher via le retour de l'execution de la fonction print de paramètre "Le joueur 1 a gagner ce round"
+            # si player2 est égal a papier
+            # alors afficher via le retour de l'execution de la fonction print de paramètre "égalité"
+            # si player2 est égal a ciseau
+            # alors afficher via le retour de l'execution de la fonction print de paramètre "Le joueur 2 a gagner ce round"
+        # si player1 est égal a ciseau
+            # si player2 est égal a pierre
+            # alors afficher via le retour de l'execution de la fonction print de paramètre "Le joueur 2 a gagner ce round"
+            # si player2 est égal a papier
+            # alors afficher via le retour de l'execution de la fonction print de paramètre "Le joueur 1 a gagner ce round"
+            # si player2 est égal a ciseau
+            # alors afficher via le retour de l'execution de la fonction print de paramètre "égalité"
+
+
+# definir une fonction joueurContreBot de paramètre pierre, papier ou ciseau
+      
 
 #FIN
