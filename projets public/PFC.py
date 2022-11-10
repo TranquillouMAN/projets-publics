e5a91480df75ceb542b0d802e34bd9915f57e923
TranquillouMAN <beney7189@gmail.com>
2022-11-10 15:28:20 +0100

    pfc finie ta mère la pute

--- projets public/PFC.py
+++ projets public/PFC.py
@@ -3,6 +3,19 @@
 # on admet la fonction random qui génère un nombre entre 0 et 2 
 # on admet la fonction input qui récup les entrés du jouer 
 
+# définir une fonction règles avec comme pramètre rules
+    # attendre 1 secondes
+    # afficher "Bienvenue dans ce magnifique shifumi"
+    # attendre 1 secondes
+    # afficher "les règles sont simples"
+    # attendre 1 secondes
+    # afficher "vous allez devoir choisir si voulez jouer contre un autre joueur ou contre un bot"
+    # attendre 1 secondes
+    # afficher "ensuite chaque joueur devras choisir pierre, papier ou ciseaux"
+    # attendre 1 secondes
+    # afficher "la pierre casse les ciseaux, la papier cache la pierre et les ciseaux coupe le papier" 
+
+
 # definir une fonction modeDeJeux avec comme paramètre joueur et bot
     # afficher via le retour de l'execution de la fonction print de paramètre "souhaitez vous jouer contre un joueur ou contre un bot"
     # assigner a la variable joueurOuBot le retour de l'execution de la fonction input 
@@ -14,20 +27,19 @@
 
 # éxécuter la fonction modeDeJeux
 
-# definir une fonction joueurContreJoueur de paramètre pierre, papier ou ciseau
-    #assigner a la variable player1 le retour de l'execution de la fonction input de paramètre "Pierre, papier ou ciseau: "
-    #assigner a la variable player2 le retour de l'execution de la fonction input de paramètre "Pierre, papier ou ciseau: "
+# definir une fonction joueurContreJoueur de paramètre pierre, papier ou ciseaux    
+    #assigner a la variable player1 le retour de l'execution de la fonction input de paramètre "Pierre, papier ou ciseaux "
+    #assigner a la variable player2 le retour de l'execution de la fonction input de paramètre "Pierre, papier ou ciseaux "
     # initialiser la variable pointPlayer1 a 0
     # initialiser la variable pointPlayer2 a 0
     #tant que pointPlayer1 ou pointPlayer2 n'est pas égal 3 
         # si player1 est égal a pierre
             # si player2 est égal a pierre
-                # alors afficher print de paramètre "égalité"
+                # alors afficher "égalité"
             # si player2 est égal a papier
                 # alors afficher "Le joueur 2 a gagner ce round"
             # assigner a la variable pointPlayer2, pointPlayer2 + 1 
-            # si player2 est égal a ciseau
-                # alors afficher "Le joueur 1 a gagner ce round"
+            # si player2 est égal a ciseaux                # alors afficher "Le joueur 1 a gagner ce round"
             # assigner a la variable pointPlayer1, pointPlayer1 + 1 
         # si player1 est égal a papier
             # si player2 est égal a pierre
@@ -35,18 +47,15 @@
             # assigner a la variable pointPlayer1, pointPlayer1 + 1 
             # si player2 est égal a papier
                 # alors afficher "égalité"
-            # si player2 est égal a ciseau
-                # alors afficher "Le joueur 2 a gagner ce round"
+            # si player2 est égal a ciseaux                # alors afficher "Le joueur 2 a gagner ce round"
             # assigner a la variable pointPlayer2, pointPlayer2 + 1 
-        # si player1 est égal a ciseau
-            # si player2 est égal a pierre
+        # si player1 est égal a ciseaux            # si player2 est égal a pierre
                 # alors afficher "Le joueur 2 a gagner ce round"
             # assigner a la variable pointPlayer2, pointPlayer2 + 1 
             # si player2 est égal a papier
                 # alors afficher "Le joueur 1 a gagner ce round"
             # assigner a la variable pointPlayer1, pointPlayer1 + 1 
-            # si player2 est égal a ciseau
-                # alors afficher "égalité"
+            # si player2 est égal a ciseaux                # alors afficher "égalité"
         # afficher pointPlayer1, '-', pointPlayer2
     # si pointPlayer1 égal a 3
         # alors afficher "le joueur 1 a gagné"
@@ -56,19 +65,19 @@
     # if restart égal a "oui"
         # alors retourner la fonction JoueurContreJoueur
 
-# definir une fonction joueurContreBot de paramètre pierre, papier ou ciseau
-    #assigner a la variable player1 le retour de l'execution de la fonction input de paramètre "Pierre, papier ou ciseau: "
-    #assigner a la variable Bot le retour de l'execution de la fonction random
+# definir une fonction joueurContreBot de paramètre pierre, papier ou ciseaux 
+    #assigner a la variable player1 le retour de l'execution de la fonction input de paramètre "Pierre, papier ou ciseaux "
+    # assigner a la variable Bot le retour de l'execution de la fonction random
     # initialiser la variable pointPlayer1 a 0
     # initialiser la variable pointBot a 0
     #tant que pointPlayer1 ou pointBot n'est pas égal 3 
         # si player1 est égal a pierre
             # si Bot est égal a 0
-                # alors afficher print de paramètre "égalité"
+                # alors afficher "égalité"
             # si Bot est égal a 1
                 # alors afficher "Le Bot a gagner ce round"
             # assigner a la variable pointBot, pointBot + 1 
-            # si Bot est égal a 3
+            # si Bot est égal a 2
                 # alors afficher "Le joueur 1 a gagner ce round"
             # assigner a la variable pointPlayer1, pointPlayer1 + 1 
         # si player1 est égal a papier
@@ -77,17 +86,16 @@
             # assigner a la variable pointPlayer1, pointPlayer1 + 1 
             # si Bot est égal a 1
                 # alors afficher "égalité"
-            # si Bot est égal a 3
+            # si Bot est égal a 2
                 # alors afficher "Le Bot a gagner ce round"
             # assigner a la variable pointBot, pointBot + 1 
-        # si player1 est égal a ciseau
-            # si Bot est égal a 0
+        # si player1 est égal a ciseaux            # si Bot est égal a 0
                 # alors afficher "Le Bot a gagner ce round"
             # assigner a la variable pointBot, pointBot + 1 
             # si Bot est égal a 1
                 # alors afficher "Le joueur 1 a gagner ce round"
             # assigner a la variable pointPlayer1, pointPlayer1 + 1 
-            # si Bot est égal a 3
+            # si Bot est égal a 2
                 # alors afficher "égalité"
         # afficher pointPlayer1, '-', pointBot
     # si pointPlayer1 égal a 3
