92d5d660875d07010b88acd042b202d81be7a60d
TranquillouMAN <beney7189@gmail.com>
2022-11-18 19:07:05 +0100

    pseudo code moitier

--- projets public/Morpions.py
+++ projets public/Morpions.py
@@ -16,136 +16,243 @@
 
 # pygame.quit()
 
+
+# assigner la variable coordonne la liste ['',"A","B","C"]
 coordonne = ['',"A","B","C"]
+# assigner a la variable ligneUn la liste ["1","","",""]
 ligneUn =["1","","",""]
+# assigner a la variable ligneDeux la liste ["2","","",""]
 ligneDeux =["2","","",""]
+# assigner a la variable ligneTrois la liste ["3","","",""]
 ligneTrois =["3","","",""]
+# initialiser la variable compteTour a 0
 compteTour = 0
+# initialiser la variable encore a 0
 encore = 0
 
+# assigner a la variable colonneUn la liste [ligneUn[1],ligneDeux[1],ligneTrois[1]]
 colonneUn = [ligneUn[1],ligneDeux[1],ligneTrois[1]]
+# assigner a la variable colonneDeux la liste [ligneUn[2],ligneDeux[2],ligneTrois[2]]
 colonneDeux = [ligneUn[2],ligneDeux[2],ligneTrois[2]]
+# assigner a la variable colonneDeux la liste [ligneUn[3],ligneDeux[3],ligneTrois[3]]
 colonneTrois = [ligneUn[3],ligneDeux[3],ligneTrois[3]]
+# assigner a la variable diagoUn la liste [colonneUn[0],colonneDeux[1],colonneTrois[2]]
 diagoUn = [colonneUn[0],colonneDeux[1],colonneTrois[2]]
+# assigner a la variable diagoDeux la liste [colonneUn[2],colonneDeux[1],colonneTrois[0]]
 diagoDeux = [colonneUn[2],colonneDeux[1],colonneTrois[0]]
 
+# initialiser la variable Jun a 0
 Jun = 0
+# initialiser la variable Jdeux a 0
 Jdeux = 0
-gagnant = 10
-
+# initialiser la variable gagnant a 3
+gagnant = 3
+# assigner a la variable Tableau la liste [coordonne,ligneUn,ligneDeux,ligneTrois]
 Tableau = [coordonne,ligneUn,ligneDeux,ligneTrois]
 
-
+# definition de la fonction Restart
 def Restart():
+    # globaliser la variable ligneDeux
     global ligneDeux
+    # globaliser la variable ligneUn
     global ligneUn
+    # globaliser la variable ligneTrois
     global ligneTrois
+    # globaliser la variable colonneDeux
     global colonneDeux
+    # globaliser la variable colonneUn
     global colonneUn
+    # globaliser la variable colonneTrois
     global colonneTrois
+    # globaliser la variable diagoDeux
     global diagoDeux
+    # globaliser la variable diagoUn
     global diagoUn
+    # globaliser la variable gagnant
     global gagnant
+    # globaliser la variable Jun
     global Jun
+    # globaliser la variable Jdeux
     global Jdeux
+    # globaliser la variable compteTour
     global compteTour
+    # globaliser la variable encore
     global encore
+    # globaliser la variableT Tableau
     global Tableau
+    # affiche "yo bande de débile tu veux retenter ?"
     print("yo bande de débile tu veux retenter ?")
+    # assigne a la variable encore le retour d'éxecutien de la fonction input de paramètre "-1 = retenter || -2 = j'abandone car jsui nul: "
     encore = input("-1 = retenter || -2 = j'abandone car jsui nul: ")
+    # si la variable encore n'est pas dans la liste ['1','2']
     if not encore in ['1','2']:
+        # alors afficher "te fous pas dma geu ptit batard mes 1 ou 2 et pas : ",encore
         print("te fous pas dma geu ptit batard mes 1 ou 2 et pas : ",encore)
-        print("yo bande de débile tu veux retenter ?")
+        # afficher "yo bande de débile vous voulez retenter ?"
+        print("yo bande de débile vous voulez retenter ?")
+        # éxécute la fonction Restart
         Restart()
+    # si la variable encore est dans la liste ['1','2']
     elif encore in ['1','2']:
+        # alors si la variable encore est = a 1
         if encore == '1':
+            # alors afficher "oki Dinosaure ON RESTARTTTTTTTRE"
             print("oki Dinosaure ON RESTARTTTTTTTRE")
+            # assigne a la variable coordone la liste ['',"A","B","C"]
             coordonne = ['',"A","B","C"]
+            # assigne a la variable ligneUn la liste ["1","","",""]
             ligneUn =["1","","",""]
+            # assigne a la variable ligneDeux la liste ["2","","",""]
             ligneDeux =["2","","",""]
+            # assigne a la variable ligneTrois la liste ["3","","",""]
             ligneTrois =["3","","",""]
+            # réinitializer a la variable compteTour a 0
             compteTour = 0
+            # réinitializer a la variable encore a 0
             encore = 0
-
+            # assigne a la variable colonneUn la liste [ligneUn[1],ligneDeux[1],ligneTrois[1]]
             colonneUn = [ligneUn[1],ligneDeux[1],ligneTrois[1]]
+             # assigne a la variable colonneDeux la liste [ligneUn[2],ligneDeux[2],ligneTrois[2]]
             colonneDeux = [ligneUn[2],ligneDeux[2],ligneTrois[2]]
+             # assigne a la variable colonneTrois la liste [ligneUn[3],ligneDeux[3],ligneTrois[3]]
             colonneTrois = [ligneUn[3],ligneDeux[3],ligneTrois[3]]
+             # assigne a la variable diagoUn la liste [colonneUn[0],colonneDeux[1],colonneTrois[2]]
             diagoUn = [colonneUn[0],colonneDeux[1],colonneTrois[2]]
+             # assigne a la variable diagoDeux la liste [colonneUn[2],colonneDeux[1],colonneTrois[0]]
             diagoDeux = [colonneUn[2],colonneDeux[1],colonneTrois[0]]
-
+            # réinitialize la variable Jun a 0
             Jun = 0
+            # réinitialize la variable Jdeux a 0
             Jdeux = 0
-            gagnant = 10
-
+            # réinitialize la variable gagnant a 3
+            gagnant = 3
+            # assigne a la variable Tableau la liste [coordonne,ligneUn,ligneDeux,ligneTrois]
             Tableau = [coordonne,ligneUn,ligneDeux,ligneTrois]
-
+            # éxecute la fonction StarterGame
             StarterGame()
-
+        # si la variable encore = a 2
         elif encore == '2':
+            # alors afficher "NNNNNNNNNNNNNNOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOBBBBBBBBBBBBBB"
             print("NNNNNNNNNNNNNNOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOBBBBBBBBBBBBBB")
+            # retourne la fonction
             return
-
+# definir la fonction winner
 def winner():
+    # globaliser la variable ligneDeux
     global ligneDeux
+    # globaliser la variable ligneUn
     global ligneUn
+    # globaliser la variable ligneTrois
     global ligneTrois
+    # globaliser la variable colonneDeux
     global colonneDeux
+    # globaliser la variable colonneUn
     global colonneUn
+    # globaliser la variable colonneTrois
     global colonneTrois
+    # globaliser la variable diagoDeux
     global diagoDeux
+    # globaliser la variable diagoUn
     global diagoUn
+    # globaliser la variable gagnant
     global gagnant
-
+    # assigne a la variable colonneUn la liste [ligneUn[1],ligneDeux[1],ligneTrois[1]]
     colonneUn = [ligneUn[1],ligneDeux[1],ligneTrois[1]]
+    # assigne a la variable colonneDeux la liste [ligneUn[2],ligneDeux[2],ligneTrois[2]]
     colonneDeux = [ligneUn[2],ligneDeux[2],ligneTrois[2]]
+    # assigne a la variable colonneTrois la liste [ligneUn[3],ligneDeux[3],ligneTrois[3]]
     colonneTrois = [ligneUn[3],ligneDeux[3],ligneTrois[3]]
+    # assigne a la variable diagoUn la liste [colonneUn[0],colonneDeux[1],colonneTrois[2]]
     diagoUn = [colonneUn[0],colonneDeux[1],colonneTrois[2]]
+    # assigne a la variable diagoDeux la liste [colonneUn[2],colonneDeux[1],colonneTrois[0]]
     diagoDeux = [colonneUn[2],colonneDeux[1],colonneTrois[0]]
-
+    # si la variable ligneUn[1], ligneUn[2], ligneUn[3] est égal a x
     if ligneUn[1] == "x" and ligneUn[2] == "x" and ligneUn[3] == "x":
+        # alors assigner a la variable gagnant, 1
         gagnant = 1
+    # si la variable ligneDeux[1], ligneDeux[2], ligneDeux[3] est égal a x
     elif ligneDeux[1] == "x" and ligneDeux[2] == "x" and ligneDeux[3] == "x":
+        # alors assigner a la variable gagnant, 1
         gagnant = 1
+    # si la variable ligneTrois[1], ligneTrois[2], ligneTrois[3] est égal a x
     elif ligneTrois[1] == "x" and ligneTrois[2] == "x" and ligneTrois[3] == "x":
+        # alors assigner a la variable gagnant, 1
         gagnant = 1
+    # si la variable colonneUn[0], colonneUn[1], colonneUn[2] est égal a x
     elif colonneUn[0] == "x" and colonneUn[1] == "x" and colonneUn[2] == "x":
+        # alors assigner a la variable gagnant, 1
         gagnant = 1
+    # si la variable colonneDeux[0], colonneDeux[1], colonneDeux[2] est égal a x
     elif colonneDeux[0] == "x" and colonneDeux[1] == "x" and colonneDeux[2] == "x":
+        # alors assigner a la variable gagnant, 1
         gagnant = 1
+    # si la variable colonneTrois[0], colonneTrois[1], colonneTrois[2] est égal a x
     elif colonneTrois[0] == "x" and colonneTrois[1] == "x" and colonneTrois[2] == "x":
+        # alors assigner a la variable gagnant, 1
         gagnant = 1
+    # si la variable diagoUn[0], diagoUn[1], diagoUn[2] est égal a x
     elif diagoUn[0] == "x" and diagoUn[1] == "x" and diagoUn[2] == "x":
+        # alors assigner a la variable gagnant, 1
         gagnant = 1
+    # si la variable diagoDeux[0], diagoDeux[1], diagoDeux[2] est égal a x
     elif diagoDeux[0] == "x" and diagoDeux[1] == "x" and diagoDeux[2] == "x":
+        # alors assigner a la variable gagnant, 1
         gagnant = 1
+    # si la variable ligneUn[1], ligneUn[2], ligneUn[3] est égal a o
     elif ligneUn[1] == "o" and ligneUn[2] == "o" and ligneUn[3] == "o":
+        # alors assigne a la variable gagnant, 2
         gagnant = 2
+    # si la variable ligneDeux[1], ligneDeux[2], ligneDeux[3] est égal a o
     elif ligneDeux[1] == "o" and ligneDeux[2] == "o" and ligneDeux[3] == "o":
+        # alors assigne a la variable gagnant, 2
         gagnant = 2
+    # si la variable ligneTrois[1], ligneTrois[2], ligneTrois[3] est égal a o
     elif ligneTrois[1] == "o" and ligneTrois[2] == "o" and ligneTrois[3] == "o":
+        # alors assigne a la variable gagnant, 2
         gagnant = 2
+    # si la variable colonneUn[0], colonneUn[1], colonneUn[2] est égal a o
     elif colonneUn[0] == "o" and colonneUn[1] == "o" and colonneUn[2] == "o":
+        # alors assigne a la variable gagnant, 2
         gagnant = 2
+    # si la variable colonneDeux[0], colonneDeux[1], colonneDeux[2] est égal a o
     elif colonneDeux[0] == "o" and colonneDeux[1] == "o" and colonneDeux[2] == "o":
+        # alors assigne a la variable gagnant, 2
         gagnant = 2
+    # si la variable colonneTrois[0], colonneTrois[1], colonneTrois[2] est égal a o
     elif colonneTrois[0] == "o" and colonneTrois[1] == "o" and colonneTrois[2] == "o":
+        # alors assigne a la variable gagnant, 2
         gagnant = 2
+    # si la variable diagoUn[0], diagoUn[1], diagoUn[2] est égal a o
     elif diagoUn[0] == "o" and diagoUn[1] == "o" and diagoUn[2] == "o":
+        # alors assigne a la variable gagnant, 2
         gagnant = 2
+    # si la variable diagoDeux[0], diagoDeux[1], diagoDeux[2] est égal a o
     elif diagoDeux[0] == "o" and diagoDeux[1] == "o" and diagoDeux[2] == "o":
+        # alors assigne a la variable gagnant, 2
         gagnant = 2
+    # si compteTour est égal a 9
     elif compteTour == 9:
+        # alors assigne a la variable gagnant, 0
         gagnant = 0
-
+# definir la fonction coupJoueur
 def coupJoueur():
+    # globaliser la variable ligneDeux
     global ligneDeux
+    # globaliser la variable ligneUn
     global ligneUn
+    # globaliser la variable ligneTrois
     global ligneTrois
+    # globaliser la variable colonneDeux
     global colonneDeux
+    # globaliser la variable colonneUn
     global colonneUn
+    # globaliser la variable colonneTrois
     global colonneTrois
+    # globaliser la variable diagoDeux
     global diagoDeux
+    # globaliser la variable diagoUn
     global diagoUn
+    # globaliser la variable gagnant
     global gagnant
     Reponse = input("mettre les coordonné : ")
     if not Reponse in ["a1","a2","a3","A1","A2","A3","b1","b2","b3","B1","B2","B3","c1","c2","c3","C1","C2","C3"]:
